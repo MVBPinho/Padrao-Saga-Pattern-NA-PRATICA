00cdeb998c4a6e89bdf0255f189cb80172a3d936
MVBPinho <marcos.vb.pinho@live.com>
2025-03-30 22:57:11 -0300

    Ajustes no serviço de estoque para orquestração

--- inventory-service/src/main/java/com/arantes/inventory/application/core/usecase/CreditInventoryUseCase.java
+++ inventory-service/src/main/java/com/arantes/inventory/application/core/usecase/CreditInventoryUseCase.java
@@ -1,10 +1,8 @@
 package com.arantes.inventory.application.core.usecase;
 
 import com.arantes.inventory.application.core.domain.Sale;
-import com.arantes.inventory.application.core.domain.enums.SaleEvent;
 import com.arantes.inventory.application.ports.in.CreditInventoryInputPort;
 import com.arantes.inventory.application.ports.in.FindInventoryByProductIdInputPort;
-import com.arantes.inventory.application.ports.out.SendToKafkaOutputPort;
 import com.arantes.inventory.application.ports.out.UpdateInventoryOutputPort;
 
 public class CreditInventoryUseCase implements CreditInventoryInputPort {
@@ -13,14 +11,10 @@ public class CreditInventoryUseCase implements CreditInventoryInputPort {
 
     private final UpdateInventoryOutputPort updateInventoryOutputPort;
 
-    private final SendToKafkaOutputPort sendToKafkaOutputPort;
-
     public CreditInventoryUseCase(FindInventoryByProductIdInputPort findInventoryByProductIdInputPort,
-                                  UpdateInventoryOutputPort updateInventoryOutputPort,
-                                  SendToKafkaOutputPort sendToKafkaOutputPort) {
+                                  UpdateInventoryOutputPort updateInventoryOutputPort) {
         this.findInventoryByProductIdInputPort = findInventoryByProductIdInputPort;
-        this.updateInventoryOutputPort = updateInventoryOutputPort;
-        this.sendToKafkaOutputPort = sendToKafkaOutputPort;
+        this.updateInventoryOutputPort = updateInventoryOutputPort;;
     }
 
     @Override
@@ -28,6 +22,5 @@ public class CreditInventoryUseCase implements CreditInventoryInputPort {
         var inventory = findInventoryByProductIdInputPort.find(sale.getProductId());
         inventory.creditQuantity(sale.getQuantity());
         updateInventoryOutputPort.update(inventory);
-        sendToKafkaOutputPort.send(sale, SaleEvent.ROLLBACK_INVENTORY);
     }
 }
--- inventory-service/src/main/java/com/arantes/inventory/config/usecase/CreditInventoryConfig.java
+++ inventory-service/src/main/java/com/arantes/inventory/config/usecase/CreditInventoryConfig.java
@@ -2,7 +2,6 @@ package com.arantes.inventory.config.usecase;
 
 import com.arantes.inventory.application.core.usecase.CreditInventoryUseCase;
 import com.arantes.inventory.application.ports.in.FindInventoryByProductIdInputPort;
-import com.arantes.inventory.application.ports.out.SendToKafkaOutputPort;
 import com.arantes.inventory.application.ports.out.UpdateInventoryOutputPort;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -12,8 +11,7 @@ public class CreditInventoryConfig {
 
     @Bean
     public CreditInventoryUseCase creditInventoryUseCase(FindInventoryByProductIdInputPort findInventoryByProductIdInputPort,
-                                                         UpdateInventoryOutputPort updateInventoryOutputPort,
-                                                         SendToKafkaOutputPort sendToKafkaOutputPort) {
-        return new CreditInventoryUseCase(findInventoryByProductIdInputPort, updateInventoryOutputPort, sendToKafkaOutputPort);
+                                                         UpdateInventoryOutputPort updateInventoryOutputPort) {
+        return new CreditInventoryUseCase(findInventoryByProductIdInputPort, updateInventoryOutputPort);
     }
 }
